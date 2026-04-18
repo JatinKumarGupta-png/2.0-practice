e292b868ca09f86c50654545ff06ad5e2bd66892
JatinKumarGupta-png <jatinbansagar117@gmail.com>
2026-04-18 12:05:26 +0530

    Like

--- script.js
+++ script.js
@@ -1,98 +1,19 @@
-const iplTeams = [
-  {
-    teamName: "MI",
-    fullName: "Mumbai Indians",
-    captain: "Hardik Pandya",
-    primaryColor: "red",
-    secondaryColor: "Gold",
-    textColor:"White"
+let img = document.querySelector('img');
+let love = document.querySelector('#love');
 
-    
+img.addEventListener('dblclick',function(){
+    love.style.opacity = 1;
+     love.style.transform = 'translate(-50%,-50%) scale(1) rotate(0deg)';
 
-  },
-  {
-    teamName: "CSK",
-    fullName: "Chennai Super Kings",
-    captain: "Ruturaj Gaikwad",
-    primaryColor: "Yellow",
-    secondaryColor: "Blue",
-    textColor:"White"
-  },
-  {
-    teamName: "RCB",
-    fullName: "Royal Challengers Bangalore",
-    captain: "Faf du Plessis",
-    primaryColor: "Red",
-    secondaryColor: "Black",
-    textColor:"White"
-  },
-  {
-    teamName: "KKR",
-    fullName: "Kolkata Knight Riders",
-    captain: "Shreyas Iyer",
-    primaryColor: "Purple",
-    secondaryColor: "Gold",
-    textColor:"White"
-  },
-  {
-    teamName: "DC",
-    fullName: "Delhi Capitals",
-    captain: "Rishabh Pant",
-    primaryColor: "black",
-    secondaryColor: "blue",
-    textColor:"White"
-  },
-  {
-    teamName: "SRH",
-    fullName: "Sunrisers Hyderabad",
-    captain: "Pat Cummins",
-    primaryColor: "Orange",
-    secondaryColor: "Black",
-    textColor:"White"
-  },
-  {
-    teamName: "RR",
-    fullName: "Rajasthan Royals",
-    captain: "Sanju Samson",
-    primaryColor: "bagani",
-    secondaryColor: "Blue",
-    textColor:"White"
-  },
-  {
-    teamName: "PBKS",
-    fullName: "Punjab Kings",
-    captain: "Shikhar Dhawan",
-    primaryColor: "Red",
-    secondaryColor: "Gold",
-    textColor:"White"
-  },
-  {
-    teamName: "GT",
-    fullName: "Gujarat Titans",
-    captain: "Shubman Gill",
-    primaryColor: "Navy Blue",
-    secondaryColor: "Gold",
-    textColor:"White"
-  },
-  {
-    teamName: "LSG",
-    fullName: "Lucknow Super Giants",
-    captain: "KL Rahul",
-    primaryColor: "Light Blue",
-    secondaryColor: "Orange",
-    textColor:"White"
-  }
-];
-var h1 = document.querySelector("h1");
-var main = document.querySelector("main");
-var btn = document.querySelector("button");
+    setTimeout(function(){
+        love.style.opacity = 0;
+        love.style.transform = 'translate(-50%,-300%) scale(0) rotate(60deg)';
 
-btn.addEventListener('click', function(){
-    var Index = Math.floor(Math.random() * iplTeams.length);
-    var team = iplTeams[Index];
-    console.log(team);
-    h1.textContent = team.teamName;
-    main.style.backgroundColor = team.secondaryColor;
-    h1.style.backgroundColor = team.primaryColor;
-   
+    },800);
+    setTimeout(function(){
+        love.style.opacity = 0;
+    },1000);
+    setTimeout(function(){
+        love.style.transform = 'translate(-50%,-50%) scale(0) rotate(60deg)';
+    },1200);
 })
